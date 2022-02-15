e4e2b28e2385e39f942d1ae66221bebaee067f5f
troberto <troberto@ACNTBFNDRQK3>
2022-02-15 15:24:26 -0300

    Add project files.

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
